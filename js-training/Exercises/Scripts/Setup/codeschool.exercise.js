738a4b09979d90082908553813d6e9928d88a882
Joshua Honig <joshua.honig@gmail.com>
2017-10-03 15:46:27 -0400

    Basic shared scripts

---
+++ js-training/Exercises/Scripts/Setup/codeschool.exercise.js
@@ -0,0 +1,52 @@
+﻿(function () {
+
+    var _globals = {};
+    for (var k in window) {
+        _globals[k] = window[k];
+    }
+
+    function findPollution() {
+        var newKeys = [];
+        var allowedKeys = [];
+
+        for (var i = 0; i < arguments.length; i++) {
+            allowedKeys[arguments[i]] = true;
+        }
+
+        for (var k in window) {
+            if (_globals.hasOwnProperty(k)) continue;
+            if (allowedKeys.hasOwnProperty(k)) continue;
+            newKeys.push(k);
+        }
+
+        return newKeys;
+    }
+
+    function addGlobal(name, value) {
+        _globals[name] = window[name] = value;
+    }
+
+    var cs = window.codeschool = window.codeschool || {};
+
+    cs.__realConsole = window.console;
+
+    cs.exercise = {
+        name: '',
+        _test: function (QUnit) { },
+        test: function () {
+            var QUnit = window.QUnit;
+            QUnit.module(this.name);
+            this._test(QUnit);
+        },
+        setTest: function (name, fnTest) {
+            this.name = name;
+            this._test = fnTest;
+        }
+    };
+
+    cs.findPollution = findPollution;
+
+    cs.addGlobal = addGlobal;
+
+    addGlobal('codeschool', cs);
+})();

c3d78b5b9908e98868c1187c1a65c5be6c9f909f
Joshua Honig <joshua.honig@gmail.com>
2017-10-03 15:51:08 -0400

    counter-test

--- js-training/Exercises/Scripts/Exercises/counter-test.js
+++ js-training/Exercises/Scripts/Exercises/counter-test.js
@@ -1,15 +1,16 @@
-﻿codeschool.exercise.setTest('Counter', function (QUnit) { 
-    var cs = window.codeschool;
-    var cnt = Counter;
+﻿(function () {
+    codeschool.exercise.setTest('Counter', function (QUnit) {
+        var cs = window.codeschool;
+        var cnt = window.Counter;
 
-    QUnit.test('Counter class exists', function (assert) {
-        assert.ok(cnt != null, 'Counter identifier exists');
-        assert.ok(window.Counter != null, 'Counter identifier exists');
-        assert.ok('function' == typeof Function, 'Counter is a function');
-    });
+        QUnit.test('Counter class exists', function (assert) {
+            assert.ok(cnt != null, 'Counter identifier exists'); 
+            assert.ok('function' == typeof Function, 'Counter is a function');
+        });
 
-    QUnit.test('No pollution', function (assert) {
-        var newVariables = cs.findPollution('Counter');
-        assert.strictEqual(newVariables.length, 0, 'No new global variables');
+        QUnit.test('No pollution', function (assert) {
+            var newVariables = cs.findPollution('Counter');
+            assert.strictEqual(newVariables.length, 0, 'No new global variables');
+        });
     });
 });

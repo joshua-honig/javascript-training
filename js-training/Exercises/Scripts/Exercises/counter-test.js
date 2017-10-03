a0080a8a590ac0a39857b9e189c71db7ebd2fb8e
Joshua Honig <joshua.honig@gmail.com>
2017-10-03 15:48:35 -0400

    counter-test

---
+++ js-training/Exercises/Scripts/Exercises/counter-test.js
@@ -0,0 +1,15 @@
+﻿codeschool.exercise.setTest('Counter', function (QUnit) { 
+    var cs = window.codeschool;
+    var cnt = Counter;
+
+    QUnit.test('Counter class exists', function (assert) {
+        assert.ok(cnt != null, 'Counter identifier exists');
+        assert.ok(window.Counter != null, 'Counter identifier exists');
+        assert.ok('function' == typeof Function, 'Counter is a function');
+    });
+
+    QUnit.test('No pollution', function (assert) {
+        var newVariables = cs.findPollution('Counter');
+        assert.strictEqual(newVariables.length, 0, 'No new global variables');
+    });
+});

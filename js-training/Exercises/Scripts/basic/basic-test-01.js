cb8f81d3da5d2c6afd68df15fc173abd6f5f7494
Joshua Honig <joshua.honig@gmail.com>
2017-10-04 08:31:07 -0400

    + hello world test

---
+++ js-training/Exercises/Scripts/basic/basic-test-01.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    codeschool.exercise.setTest('Hello world', function (QUnit) {
+        var cs = window.codeschool;
+
+        QUnit.test('logged Hello World!', function (assert) {
+            var lines = cs.console.getLines();
+            assert.strictEqual(lines.length, 1, 'Logged 1 line');
+            assert.strictEqual(lines[0], 'Hello world!', 'Logged \'Hello world!\'');
+        });
+    });
+})();
+
+setTimeout(function () { codeschool.exercise.test(); }, 0);

8d2b768ec706fafb735396fbe8cdbb7dd5c281b4
Joshua Honig <joshua.honig@gmail.com>
2017-10-03 22:46:54 -0400

    Added string utils test 2

---
+++ js-training/Exercises/Scripts/stringUtils/stringUtils-solution-02.js
@@ -0,0 +1,55 @@
+﻿// Instructions: 
+// Augment the existing nifty.utis.str module, without exporting anything globally 
+// - Add the single static function 'pad' with the signature: pad(value: string, length: number, char?: string): string  
+//   - pads the input value to the specified length, using the pad character if supplied, otherwise space
+//   - if length is negative, pad left-pads to the absolute value of length 
+//   - pad should internally call the *alread-defined* method nifty.utils.str.repeat, which has the signature: repeat(value: string, count: number): string
+//   - null or undefined value should be treated as empty string
+//   - any other non-string argument should be converted to string with String(value) function
+//   - other requirements are described in the unit test results...
+//   - Examples:
+//       stringUtils.pad('hello', 1)         // 'hello'
+//       stringUtils.pad('hello', 8)         // 'hello   '
+//       stringUtils.pad('hello', -8)        // '   hello'
+//       stringUtils.pad('hello', 8, '_')    // 'hello___'
+//       stringUtils.pad('hello', 8, 'foo')  // 'hellofff'
+//       stringUtils.pad(null, 2)            // '  '
+//       stringUtils.pad('hello')            // throws Error
+//       stringUtils.pad('hello', 'foo')     // throws TypeError
+
+(function () {
+
+    var str = nifty.utils.str;
+    var repeat = str.repeat;
+
+    function pad(value, length, char) {
+        if (value == null) value = '';
+        if (arguments.length < 2) throw new Error('Pad requires at least two arguments');
+        length = Math.ceil(+length);
+        if (isNaN(length)) throw new TypeError('length must be a number');
+
+        if (arguments.length >= 3) {
+            if ('string' != typeof char) throw new TypeError('char must be a string');
+            if (('string' == typeof char) && char.length == 0) throw new Error('char cannot be an empty string');
+            char = char.charAt(0);
+        } else {
+            char = ' ';
+        }
+
+        if ('string' != typeof value) value = String(value);
+
+        var absLength = Math.abs(length);
+        if (value.length >= absLength) return value;
+
+        var padString = repeat(char, absLength - value.length);
+        if (length < 0) {
+            value = padString + value;
+        } else {
+            value += padString;
+        }
+
+        return value;
+    }
+
+    str.pad = pad;
+})();
---
+++ js-training/Exercises/Scripts/stringUtils/stringUtils-test-02.js
@@ -0,0 +1,65 @@
+﻿(function () {
+
+    var _repeatCalled = 0;
+
+    var nifty_module = {
+        utils: {
+            name: 'nifty utils',
+            str: {
+                repeat: function (value, count) {
+                    _repeatCalled++;
+                    var result = '';
+                    for (var i = 0; i < count; i++) { result += value; }
+                    return result;
+                }
+            }
+        }
+    };
+
+    window.nifty = nifty_module;
+     
+    codeschool.exercise.setTest('nifty.utils.str', function (QUnit) {
+        var cs = window.codeschool;
+        var nifty = window.nifty;
+        var $ = window.jQuery;
+
+        QUnit.test('No pollution', function (assert) {
+            var newVariables = cs.findPollution('nifty');
+            assert.strictEqual(newVariables.length, 0, 'No new global variables');
+        });
+
+        QUnit.test('namespaces were augmented', function (assert) {
+            assert.strictEqual(nifty, nifty_module, 'nifty wasn\'t clobbered');
+            assert.strictEqual(nifty.utils.name, 'nifty utils', 'nifty.utils wasn\'t clobbered');
+            assert.strictEqual(nifty.utils.str.name, 'nifty string utils', 'nifty.utils.str wasn\'t clobbered');
+            assert.strictEqual(nifty.utils.str.repeat, nifty.__str_repeat, 'nifty.utils.str.repeat wasn\'t clobbered');
+        });
+         
+        QUnit.test('pad', function (assert) {
+            var pad = nifty.utils.str.pad;
+            _repeatCalled = 0;
+
+            assert.ok($.isFunction(pad), 'nifty.utils.str.pad is a function'); 
+
+            assert.throws(function () { pad('foo'); }, 'pad throws error if only 1 argument');
+            assert.throws(function () { pad('foo', 'a'); }, 'pad throws error if length is NaN or not a number');
+            assert.throws(function () { pad('foo', 'a', {}); }, 'pad throws error if pad char is not a string');
+            assert.throws(function () { pad('foo', 'a', ''); }, 'pad throws error if pad char is an empty string');
+
+            assert.strictEqual(pad(null, 5), '     ', 'null value treated as empty string');
+            assert.strictEqual(pad(undefined, 5), '     ', 'undefined value treated as empty string');
+            assert.strictEqual(pad('foo', 5), 'foo  ', 'Basic usage');
+            assert.strictEqual(pad('foo', -5), '  foo', 'Basic usage, left aligned');
+            assert.strictEqual(pad('foo', 2), 'foo', 'If width is less than length, input is returned');
+            assert.strictEqual(pad('foo', 5, '_'), 'foo__', 'Custom pad char');
+            assert.strictEqual(pad('foo', -5, '_'), '__foo', 'Custom pad char, left aligned');
+            assert.strictEqual(pad('foo', 5, '_!='), 'foo__', 'Only first char of custom padding is used');
+            assert.strictEqual(pad(123, 5, '0'), '12300', 'number value is converted to string');
+            assert.strictEqual(pad(window, 5), '[object Window]', 'object value is converted to string');
+
+            assert.strictEqual(_repeatCalled, 8, 'nifty.utils.str.repeat was called 8 times by these tests');
+        });
+    });
+})();
+
+setTimeout(function () { codeschool.exercise.test(); }, 0);
